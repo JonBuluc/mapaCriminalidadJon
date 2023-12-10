4362f7e67edf362d245f326f15879aa796415f53
JonBuluc <jonanb515@gmail.com>
2023-12-10 16:28:39 -0600

    Se agrego un boton que permite ver las denuncias por delitos

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import dash
-from dash import dcc, html 
+from dash import dcc, html, callback, Input, Output
 import pandas as pd
 import plotly.express as px
 
@@ -10,22 +10,60 @@ df = pd.read_csv('carpetasFGJ_2023.csv')  # Asegúrate de que el nombre del arch
 df = df.dropna(subset=['latitud', 'longitud'])
 
 # Crear la figura del mapa de calor
-fig = px.density_mapbox(df, lat='latitud', lon='longitud', 
-                        radius=10, 
+fig = px.density_mapbox(df, lat='latitud', lon='longitud',
+                        radius=10,
                         center={"lat": 19.36, "lon": -99.133209},  # Centro en la Ciudad de México
-                        zoom=10, 
+                        zoom=10,
                         mapbox_style="mapbox://styles/mapbox/light-v10")  # Puedes cambiar el estilo del mapa aquí
 
-fig.update_layout(mapbox_accesstoken='tu_clave_aqui')
+# Lee la clave API desde el archivo
+with open('api_key.txt', 'r') as file:
+    key = file.read().strip()
+fig.update_layout(mapbox_accesstoken=key)
 
 # Iniciar la aplicación Dash
 app = dash.Dash(__name__)
 
+# Definir las opciones para el menú desplegable
+categorias_delito = df['delito'].unique()
+opciones_delito = [{'label': categoria, 'value': categoria} for categoria in categorias_delito]
+
 # Definir el layout de la aplicación
 app.layout = html.Div([
     html.H1("Mapa de Calor de Criminalidad en la Ciudad de México", className='h1'),
-    dcc.Graph(figure=fig, className='graph')
+    
+    # Menú desplegable para seleccionar la categoría del delito
+    dcc.Dropdown(
+        id='dropdown-delito',
+        options=opciones_delito,
+        value=categorias_delito[0],  # Valor predeterminado
+        multi=False,
+        style={'width': '50%'}
+    ),
+    
+    dcc.Graph(id='mapa-delito', className='graph')
 ], className='body')
+
+# Callback para actualizar el mapa cuando se selecciona una categoría de delito
+@app.callback(
+    Output('mapa-delito', 'figure'),
+    [Input('dropdown-delito', 'value')]
+)
+def actualizar_mapa(categoria_delito):
+    # Filtrar el DataFrame para mostrar solo la categoría de delito seleccionada
+    df_filtrado = df[df['delito'] == categoria_delito]
+
+    # Crear la figura del mapa de calor actualizada
+    fig_actualizada = px.density_mapbox(df_filtrado, lat='latitud', lon='longitud',
+                                        radius=10,
+                                        center={"lat": 19.36, "lon": -99.133209},  # Centro en la Ciudad de México
+                                        zoom=10,
+                                        mapbox_style="mapbox://styles/mapbox/light-v10")
+
+    # Actualizar la clave API del mapa
+    fig_actualizada.update_layout(mapbox_accesstoken=key)
+
+    return fig_actualizada
 
 # Ejecutar la aplicación
 if __name__ == '__main__':
